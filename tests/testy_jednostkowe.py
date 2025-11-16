f6628675d5e73eab3e3c54e61b05639ac4649525
“Marek <marek.szafron@edu.uekat.pl>
2025-11-16 11:41:29 +0100

    86c6hx0de

--- tests/testy_jednostkowe.py
+++ tests/testy_jednostkowe.py
@@ -1,5 +1,6 @@
 import pytest
 from testy_jednostkowe_log import is_palindrome
+from testy_jednostkowe_log import fibonacci
 
 @pytest.mark.parametrize("text, expected", [
     ("kajak", True),
@@ -10,3 +11,17 @@ from testy_jednostkowe_log import is_palindrome
 ])
 def test_is_palindrome(text, expected):
     assert is_palindrome(text) == expected
+
+@pytest.mark.parametrize('n, expected',  [
+    (0, 0),
+    (1, 1),
+    (5,5),
+    (10, 55),
+    (-1, ValueError)
+])
+def test_fibonacci(n, expected):
+    if expected is ValueError:
+        with pytest.raises(ValueError):
+            fibonacci(n)
+    else:
+        assert fibonacci(n) == expected
--- testy_jednostkowe_log.py
+++ testy_jednostkowe_log.py
@@ -1,3 +1,14 @@
 def is_palindrome(tekst:str)->bool:
     cleaned = tekst.replace(" ", "").lower()
     return cleaned == cleaned[::-1]
+
+def fibonacci(n: int) -> int:
+    if n < 0:
+        raise ValueError
+    if n in (0, 1):
+        return n
+
+    a, b = 0, 1
+    for _ in range(2, n + 1):
+        a, b = b, a + b
+    return b
